818b396009be432e531af9a0b73510070c5e5993
Yist Lin <yishen992@gmail.com>
2019-02-22 10:25:36 +0100

    successfully decipher url version 1

--- decipher.go
+++ decipher.go
@@ -1,13 +1,12 @@
 package megadecipher
 
 import (
-	// "fmt"
-	"errors"
-	"regexp"
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/hex"
 	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"regexp"
 )
 
 var (
@@ -37,32 +36,21 @@ func Decipher(url string) (string, error) {
 		rootUrl += "F"
 	}
 
-	// decipher url v2
+	var key []byte
 	if versiontag == "2" {
-		deciphertext, err := decipherV2(b64str)
-		if err != nil {
-			return "", err
+		key = []byte{237, 31, 76, 32, 11, 53, 19, 152, 6, 178, 96, 86, 59, 61, 56, 118, 240, 17, 180, 117, 15, 58, 26, 74, 94, 253, 11, 190, 103, 85, 75, 68}
+	} else {
+		password := []rune{'k', '1', 'o', '6', 'A', 'l', '-', '1', 'k', 'z', '?', '!', 'z', '0', '5', 'y'}
+		for len(password) < 32 {
+			password = append(password, 'X')
 		}
-		return rootUrl + deciphertext, nil
+		key = []byte(string(password))
 	}
 
-	// decipher url v1
-	// TODO
-	return url, nil
-}
-
-func decipherV1(b64str string) (string, error) {
-	// key := "k1o6Al-1kz¿!z05y"
-	// iv, _ := hex.DecodeString("79F10A01844A0B27FF5B2D4E0ED3163E")
-	return b64str, nil
-}
-
-func decipherV2(b64str string) (string, error) {
-	key := []byte{237, 31, 76, 32, 11, 53, 19, 152, 6, 178, 96, 86, 59, 61, 56, 118, 240, 17, 180, 117, 15, 58, 26, 74, 94, 253, 11, 190, 103, 85, 75, 68}
 	iv, _ := hex.DecodeString("79F10A01844A0B27FF5B2D4E0ED3163E")
 
-	// get ciphered text
-	b64str += ("==")[((2 - len(b64str) * 3) & 3):]
+	// decode base64 encoded url
+	b64str += ("==")[((2 - len(b64str)*3) & 3):]
 	ciphertext, err := base64.URLEncoding.DecodeString(b64str)
 	if err != nil {
 		return "", err
@@ -74,7 +62,7 @@ func decipherV2(b64str string) (string, error) {
 	mode.CryptBlocks(ciphertext, ciphertext)
 
 	// unpad(pksc7)
-	c := ciphertext[len(ciphertext) - 1]
+	c := ciphertext[len(ciphertext)-1]
 	n := int(c)
 	if n == 0 || n > len(ciphertext) {
 		return "", ErrInvalidPKCS7Padding
@@ -86,5 +74,6 @@ func decipherV2(b64str string) (string, error) {
 	}
 
 	deciphertext := string(ciphertext[:len(ciphertext)-n])
-	return deciphertext, nil
+
+	return rootUrl + deciphertext, nil
 }
